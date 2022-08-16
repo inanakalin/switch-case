c6a4859c0205cd9776ba163c134052735a9a635b
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-08-16 23:45:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      int month = DateTime.Now.Month;
+
+      //Expressions
+
+      switch (month)
+      {
+        case 1:
+            Console.WriteLine("Ocak Ayındasınız");
+            break;
+        case 2:
+            Console.WriteLine("Şubat Ayındasınız");
+            break;
+        case 8:
+            Console.WriteLine("Ağustos Ayındasınız");
+            break;
+       
+        default:
+            Console.WriteLine("Yanlış veri");
+
+        break;
+
+      }
+        switch (month)
+        {
+            case 12: case 1: case 2:
+            Console.WriteLine("Kış Mevsimindesiniz");
+            break;
+            case 3: case 4: case 5:
+            Console.WriteLine("İlkbahar Mevsimindesiniz");
+            break;
+            case 6: case 7: case 8:
+            Console.WriteLine("Yaz Mevsimindesiniz");
+            break;
+            case 9: case 10: case 11:
+            Console.WriteLine("Sonbahar Mevsimindesiniz");
+            break;
+            
+            default:
+            break;
+        }
+
+
+
+      
+    }
+}
